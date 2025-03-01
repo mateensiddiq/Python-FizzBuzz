ae827c7d0aa6ab0db6bfa3787aee95f0f769de61
Mateen Siddiq <76666491+mateensiddiq@users.noreply.github.com>
2025-03-01 07:43:32 -0500

    Added Inputs to allow user to customize beginning and end point

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -2,10 +2,10 @@
 #But, if a number is a multiple of 3, we print Fizz.
 #If a number is a multiple of 5, we print Buzz.
 #If a number is a multiple of both 3 and 5, then we print FizzBuzz
-i=1
+x = int(input("What is the start number of FizzBuzz?"))
+n = int(input("What is the end number of FizzBuzz?"))
 
-
-for i in range(1,101):
+for i in range(x,n+1):
 
     if(i%3==0 and i%5==0):
         print("FizzBuzz")
